98f4b6afd98dedc52881fcff92bd845a3da32563
unknown <Suporte@STFSAON006140-L.stefanini.dom>
2022-08-28 20:27:02 -0300

    more 10 challenges

---
+++ desafio_v6.py
@@ -0,0 +1,7 @@
+# Crie um algoritmo que leia um número
+# e mostre o seu dobro, o triplo e a raiz quadrada
+
+num = float(input('Digite um número: '))
+print('O dobro do número é: ', num * 2)
+print('O triplo do número é: ', num * 3)
+print('A raiz quadrada do número é: ', (num ** 0.5))
