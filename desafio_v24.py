4986b995f877fd8b6c9d14719b194f2bdbf093da
wlaurentino <gama.william@gmail.com>
2022-09-08 22:53:45 -0300

    challenges 23 and 24

---
+++ desafio_v24.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia o nome de uma cidade
+# e diga se ela começa ou não com o nome "SANTO".
+
+cidade = str(input('Digite o nome da cidade: '))
+
+while True:
+    if 'SANTO' and 'SÃO' in cidade.upper():
+        print('A cidade: {}, tem nome de SANTO'.format(cidade))
+    else:
+        print('A cidade: {}, não tem nome de SANTO'. format(cidade))
+    break
