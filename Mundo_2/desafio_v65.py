e6be65f07ffc7fa69c15b63092e426d84a2499d8
wlaurentio <gama.william@gmail.com>
2022-10-27 07:48:01 -0300

    adding challenge 65

---
+++ Mundo_2/desafio_v65.py
@@ -0,0 +1,28 @@
+# Crie um programa que leia vários números inteiros pelo teclado.
+# No final da execução, mostre a média entre todos os valores
+# e qual foi o maior e o menor valores lidos.
+# O programa deve perguntar ao usuário se ele quer
+# ou não continuar a digitar valores.
+
+cont = 0
+soma = 0
+maior = 0
+menor = 0
+sair = 'N'
+
+while sair != 'S':
+    num = int(input('Digite um número: '))
+    cont += 1
+    soma = num + num
+    if cont == 1:
+        maior = num
+        menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+            menor = num
+    sair = str(input('Para sair do programa digite: [S/N] ')).strip().upper()
+print("""Você digitou {} números e a média entre eles foi {}
+O maior valor foi {} e o menor foi {}"""
+      .format(cont, soma/cont, maior, menor))
