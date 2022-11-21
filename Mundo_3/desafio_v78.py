3351273314d7d6b916f6a57f8ef09877fb07da6c
wlaurentio <gama.william@gmail.com>
2022-11-21 17:01:11 -0300

    improving challenge 78

--- Mundo_3/desafio_v78.py
+++ Mundo_3/desafio_v78.py
@@ -9,11 +9,11 @@ menor = 0
 valores = list()
 for cont in range(0, 5):
     valores.append(int(input(f'Digite um valor para a posição {cont}: ')))
-print(f'O maior valor digitado é: {max(valores)} nas posições... ', end='')
+print(f'O maior valor digitado é: {max(valores)} nas posições...', end='')
 for c, v in enumerate(valores):
     if v == max(valores):
         print(f'{c}...', end='')
-print(f'O menor valor digitado é: {min(valores)} nas posições... ', end='')
+print(f'\nO menor valor digitado é: {min(valores)} nas posições...', end='')
 for c, v in enumerate(valores):
     if v == min(valores):
         print(f'{c}...', end='')
